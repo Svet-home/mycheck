d0838ed11f34f092d5ae540013f9eebf505a2b4c
Svet-home <aprelkovakatish@gmail.com>
2022-12-07 13:47:05 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import image from './smile.jpg';
+
+import { GroceryList } from './GroceryList';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='container'>
+<img src={ image } alt="Happy smile" width="300px" className='smile'/>
+</div>
+<div className='container'>
+<h1>Список продуктов</h1>
+</div>
+<GroceryList/>
+<div className='container'>
+
+</div>
     </div>
   );
 }
